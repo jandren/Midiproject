6659f2eefeadd220107dc183c67dd52db9d32744
jandren <andren.jakob@gmail.com>
2015-11-23 14:13:25 -0500

    Added MIDI_send

--- Midiproject/src/main.c
+++ Midiproject/src/main.c
@@ -27,11 +27,16 @@
 void UART_out(uint8_t ch);
 void Init_UART0(uint16_t BAUDRATE);
 void Init_ports(void);
-uint8_t Conversion(uint8_t pressed);
+
+// For avrMIDI.c file
+uint8_t MIDI_Conversion(uint8_t pressed);
+void MIDI_send(uint8_t command, uint8_t tone);
+
 
 // global variables
 volatile uint16_t rx_ch = 0xFF;
 volatile uint8_t switches = 0x00; // Byte containing [rec, play1, play2, play3, ch_bit0, ch_bit1, pitch_bit0, pitch_bit1];
+volatile uint8_t volume = 100;
 
 int main(void)
 {
@@ -43,6 +48,10 @@ int main(void)
 	uint8_t previus = 0x00;
 	uint8_t current = 0x00;
 	uint8_t change  = 0x00;
+	
+	// MIDI related variables
+	uint8_t command = 0;
+	uint8_t tone = 0;
 	
 	while
 	(1)
@@ -56,21 +65,43 @@ int main(void)
 		
 		if(current & change) // Note on
 		{
-			// I want add stuff here
-			UART_out(0b10010001); // Command
-			UART_out(60+Conversion(current & change)); // Note 7bit
-			UART_out(80 ); //volume(switches & 0x03)); // Velocity 7 bit
+			command = 0b10010000 | (switches & 0x0C) >> 2;
+			tone = MIDI_Conversion(current & change);
+			
+			if(switches & 0x80){
+				//REC_add(command, tone);
+			}
+			
+			MIDI_send(command, tone);
+			
+			//UART_out(0b10010001); // Command
+			//UART_out(60+Conversion(current & change)); // Note 7bit
+			//UART_out(80 ); //volume(switches & 0x03)); // Velocity 7 bit
 		}
 		else if(previus & change) // Note off
 		{
+			
+			
+			
+			
 			UART_out(0b10000001); // Command
-			UART_out(60+Conversion(previus & change)); // Note 7bit
+			UART_out(60+MIDI_Conversion(previus & change)); // Note 7bit
 			UART_out(0b01001000); // Velocity 7 bit
 		}
 		previus = current;
 		//_delay_ms(300);
 	}
 	// End of while(1)
+}
+
+void MIDI_send(uint8_t command, uint8_t tone)
+// Send the Midi command, not much really but abstracts the code a little.
+{
+	UART_out(command); // Command 7-bit
+	UART_out(tone); // Note 7bit
+	UART_out(volume >> 1); // Velocity 7 bit from 8 bit volume
+	
+	// See Midi command table for exact bits to input.
 }
 
 ISR(USART0_RX_vect){
@@ -112,12 +143,13 @@ void Init_ports(void){
 	PORTD = 0xFF; // Set pull up on the inputs
 }
 
-uint8_t Conversion(uint8_t pressed)
+uint8_t MIDI_Conversion(uint8_t pressed)
 {
-	uint8_t converted = 48;
+	uint8_t converted = 0;
 	uint8_t offset = 48;
 	uint8_t pitch = switches & 0x03;		// mask with two far right switches
 
+/*
 	if( pressed &  0x01){
 		 converted = offset + 0 + 12 * pitch;
 		 			// C4 = 48 or C5 = 60 or C6 = 72
@@ -139,37 +171,37 @@ uint8_t Conversion(uint8_t pressed)
 		 			// B4 = 59 or B5 = 71 or B6 = 83
 	if( pressed &  0x80) converted = offset + 12 + 12 * pitch;
 		 			// C5 = 60 or C6 = 72 or C7 = 84
+*/
 
-
-/*	switch(pressed)
+	switch(pressed)
 	{
 		case 0x01:
-		converted = 48 + 12 * switches;
+		converted = 48 + 12 * pitch;
 		break;				// C4 = 48 or C5 = 60 or C6 = 72
 		case 0x02:
-		converted = 50 + 12 * switches;
+		converted = 50 + 12 * pitch;
 		break;				// D4 = 50 or D5 = 62 or D6 = 74
 		case 0x04:
-		converted = 52 + 12 * switches;
+		converted = 52 + 12 * pitch;
 		break;				// E4 = 52 or E5 = 64 or E6 = 76
 		case 0x08:
-		converted = 53 + 12 * switches;
+		converted = 53 + 12 * pitch;
 		break;				// F4 = 53 or F5 = 65 or F6 = 77
 		case 0x10:
-		converted = 55 + 12 * switches;
+		converted = 55 + 12 * pitch;
 		break;				// G4 = 55 or G5 = 67 or G6 = 79
 		case 0x20:
-		converted = 57 + 12 * switches;
+		converted = 57 + 12 * pitch;
 		break;				// A4 = 57 or A5 = 69 or A6 = 81
 		case 0x40:
-		converted = 59 + 12 * switches;
+		converted = 59 + 12 * pitch;
 		break;				// B4 = 59 or B5 = 71 or B6 = 83
 		case 0x80:
-		converted = 60 + 12 * switches;
+		converted = 60 + 12 * pitch;
 		break;				// C5 = 60 or C6 = 72 or C7 = 84
 		default:
 		break;
-	}*/
+	}
 	
 	return converted;
 }
