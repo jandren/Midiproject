5db000a651f3ab6e0edfbfa7d2ff9f9a7cf082bb
sjeom0519 <eoms@purdue.edu>
2015-12-11 12:48:20 -0500

    avrMIDI comments

--- Midiproject/src/avrMIDI.c
+++ Midiproject/src/avrMIDI.c
@@ -3,8 +3,17 @@
  *
  * Includes UART and MIDI instructions
  *
+ * Functions:
+ * Initialization of ports				(Init_ports)
+ * Initialization of UART				(Init_UART0)
+ * Read from a pin for UART				(UART_out)
+ * Send a command through UART			(MIDI_send)
+ * Conversion of music note from input	(MIDI_Conversion)
+ * Initialization of ADC				(init_adc)
+ * initialization of Timer0				(init_Timer0)
+ *
  * Created: 2015-11-23 11:31:25
- *  Author: ASUS
+ *  Author: Jakob Andren & SJ
  */
 
 #include "avrMIDI.h"
@@ -72,9 +81,9 @@ void MIDI_send(uint8_t command, uint8_t tone, uint8_t volume)
 
 uint8_t MIDI_Conversion(uint16_t pressed)
 {
-	uint16_t converted = 0;
-	uint8_t offset = 48;
-	uint8_t pitch = switches & 0x03;		// mask with two far right switches
+	uint16_t converted = 0;				// music note
+	uint8_t offset = 48;				// start from C4 = 48
+	uint8_t pitch = switches & 0x03;	// mask with two far right switches
 	
 	if(switches & 0b00010000){  // Only flats
 	
@@ -106,28 +115,28 @@ uint8_t MIDI_Conversion(uint16_t pressed)
 			break;				// C5 = 60 or C6 = 72 or C7 = 84
 			case 0x0100:
 			converted = 62 + 12 * pitch;
-			break;				// C4 = 48 or C5 = 60 or C6 = 72
+			break;				// D5 = 62 or D6 = 74 or D7 = 86
 			case 0x0200:
 			converted = 64 + 12 * pitch;
-			break;				// D4 = 50 or D5 = 62 or D6 = 74
+			break;				// E5 = 64 or E6 = 76 or E7 = 88
 			case 0x0400:
 			converted = 65 + 12 * pitch;
-			break;				// E4 = 52 or E5 = 64 or E6 = 76
+			break;				// F5 = 65 or F6 = 77 or F7 = 89
 			case 0x0800:
 			converted = 67 + 12 * pitch;
-			break;				// F4 = 53 or F5 = 65 or F6 = 77
+			break;				// G5 = 67 or G6 = 79 or G7 = 91
 			case 0x1000:
 			converted = 69 + 12 * pitch;
-			break;				// G4 = 55 or G5 = 67 or G6 = 79
+			break;				// A5 = 69 or A6 = 81 or A7 = 93
 			case 0x2000:
 			converted = 71 + 12 * pitch;
-			break;				// A4 = 57 or A5 = 69 or A6 = 81
+			break;				// B5 = 71 or B6 = 83 or B7 = 95
 			case 0x4000:
 			converted = 72 + 12 * pitch;
-			break;				// B4 = 59 or B5 = 71 or B6 = 83
+			break;				// C6 = 72 or C7 = 84 or C8 = 96
 			case 0x8000:
 			converted = 74 + 12 * pitch;
-			break;				// C5 = 60 or C6 = 72 or C7 = 84
+			break;				// D6 = 74 or D7 = 86 or D8 = 98
 			default:
 			break;
 		}
@@ -137,52 +146,52 @@ uint8_t MIDI_Conversion(uint16_t pressed)
 		{
 			case 0x0001:
 			converted = offset + 0 + 12 * pitch;
-			break;				// C4 = 48 or C5 = 60 or C6 = 72
+			break;				// C4 = 48 or E5 = 64 or G6# = 80
 			case 0x0002:
 			converted = offset + 1 + 12 * pitch;
-			break;				// D4 = 50 or D5 = 62 or D6 = 74
+			break;				// C4# = 49 or F5 = 65 or A6 = 81
 			case 0x0004:
 			converted = offset + 2 + 12 * pitch;
-			break;				// E4 = 52 or E5 = 64 or E6 = 76
+			break;				// D4 = 50 or F5# = 66 or A6# = 82
 			case 0x0008:
 			converted = offset + 3 + 12 * pitch;
-			break;				// F4 = 53 or F5 = 65 or F6 = 77
+			break;				// D4# = 51 or G5 = 67 or B6 = 83
 			case 0x0010:
 			converted = offset + 4 + 12 * pitch;
-			break;				// G4 = 55 or G5 = 67 or G6 = 79
+			break;				// E4 = 52 or G5# = 68 or C7 = 84
 			case 0x0020:
 			converted = offset + 5 + 12 * pitch;
-			break;				// A4 = 57 or A5 = 69 or A6 = 81
+			break;				// F4 = 53 or A5 = 69 or C7# = 85
 			case 0x0040:
 			converted = offset + 6 + 12 * pitch;
-			break;				// B4 = 59 or B5 = 71 or B6 = 83
+			break;				// F4# = 54 or A5# = 70 or D7 = 86
 			case 0x0080:
 			converted = offset + 7 + 12 * pitch;
-			break;				// C5 = 60 or C6 = 72 or C7 = 84
+			break;				// G4 = 55 or B5 = 71 or D7# = 87
 			case 0x0100:
 			converted = offset + 8 + 12 * pitch;
-			break;				// C4 = 48 or C5 = 60 or C6 = 72
+			break;				// G4# = 56 or C6 = 72 or E7 = 88
 			case 0x0200:
 			converted = offset + 9 + 12 * pitch;
-			break;				// D4 = 50 or D5 = 62 or D6 = 74
+			break;				// A4 = 57 or C6# = 73 or F7 = 89
 			case 0x0400:
 			converted = offset + 10 + 12 * pitch;
-			break;				// E4 = 52 or E5 = 64 or E6 = 76
+			break;				// A4# = 58 or D6 = 74 or F7# = 90
 			case 0x0800:
 			converted = offset + 11 + 12 * pitch;
-			break;				// F4 = 53 or F5 = 65 or F6 = 77
+			break;				// B4 = 59 or D6# = 75 or G7 = 91
 			case 0x1000:
 			converted = offset + 12 + 12 * pitch;
-			break;				// G4 = 55 or G5 = 67 or G6 = 79
+			break;				// C5 = 60 or E6 = 76 or G7# = 92
 			case 0x2000:
 			converted = offset + 13 + 12 * pitch;
-			break;				// A4 = 57 or A5 = 69 or A6 = 81
+			break;				// C5# = 61 or F6 = 77 or A7 = 93
 			case 0x4000:
 			converted = offset + 14 + 12 * pitch;
-			break;				// B4 = 59 or B5 = 71 or B6 = 83
+			break;				// D5 = 62 or F6# = 78 or A7# = 94
 			case 0x8000:
 			converted = offset + 15 + 12 * pitch;
-			break;				// C5 = 60 or C6 = 72 or C7 = 84
+			break;				// D5# = 63 or G6 = 79 or B7 = 95
 			default:
 			break;
 			}
@@ -199,7 +208,7 @@ void init_adc(void)					// function for initializing ADC
 
 ISR(ADC_vect)						// read ADC using interrupt
 {
-	volume = ADCH;									// save 8 bits
+	volume = ADCH;										// save 8 bits
 	ADMUX = (ADMUX & 0xE0) | ((switches & 0x0C) >> 2);	// Mask and shift channel bits from switches
 }
 
@@ -207,12 +216,12 @@ void init_Timer0(void)
 {
 	TCCR0A = 0b00000000;			// normal mode
 	TCCR0B = 0b00000101;			// prescaler 1024
-	TCNT0 = 0;					// overflow, 255*1024/(8*10^6) = 32.6ms delay at max
+	TCNT0 = 0;						// overflow, 255*1024/(8*10^6) = 32.6ms delay at max
 	TIMSK0 |= (1 << TOIE0);			// enable overflow interrupt enable
 }
 
 ISR(TIMER0_OVF_vect)
 {
-	ADCSRA |= (1<<ADSC);
+	ADCSRA |= (1<<ADSC);			// start the ADC conversion
 	//TCNT0 = 0;					// reset the count TCNT0
 }
